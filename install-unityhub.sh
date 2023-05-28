3f482e8ec5e8642d75947296b4ca045197582206
Dann Herz <danielhdzmzin@gmail.com>
2023-05-28 16:00:48 -0600

    Update install-unityhub.sh

--- install-unityhub.sh
+++ install-unityhub.sh
@@ -1,10 +1,10 @@
-#!/usr/env/bash
+#!/bin/bash
 
 # Install the gpg public key
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/unityhub.gpg > /dev/null # Redirect the tee stdout to /dev/null; creating the file only
 
 # Install the unityhub repo into the apt source list directory
-sudo echo "deb [signed-by=/usr/share/keyrings/unityhub.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list
+echo "deb [signed-by=/usr/share/keyrings/unityhub.gpg] https://hub.unity3d.com/linux/repos/deb stable main" | sudo tee /etc/apt/sources.list.d/unityhub.list > /dev/null
 
 # Update apt's repository list and install unity hub
 sudo apt update
